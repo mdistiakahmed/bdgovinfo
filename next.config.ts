37d8c013d3bb280529924098c67de73032a3f7f0
Md Istiak Ahmed <istinishat@gmail.com>
2026-05-14 17:17:02 +0700

    remove next config

--- next.config.ts
+++ next.config.ts
@@ -1,40 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:lang(en|bn)/birth-certificate/check",
-        destination: "/:lang/digital-birth-certificate-check-bangladesh",
-        permanent: true,
-      },
-      {
-        source: "/:lang(en|bn)/birth-certificate/registration",
-        destination: "/:lang/digital-birth-certificate-registration-bangladesh",
-        permanent: true,
-      },
-      {
-        source: "/:lang(en|bn)/nid/check",
-        destination: "/:lang/nid-check-bangladesh",
-        permanent: true,
-      },
-      {
-        source: "/:lang(en|bn)/nid/registration",
-        destination: "/:lang/nid-registration-bangladesh",
-        permanent: true,
-      },
-      {
-        source: "/:lang(en|bn)/passport/check",
-        destination: "/:lang/passport-check-bangladesh",
-        permanent: true,
-      },
-      {
-        source: "/:lang(en|bn)/passport/registration",
-        destination: "/:lang/passport-registration-bangladesh",
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
