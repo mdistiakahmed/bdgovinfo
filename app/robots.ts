f0ac0929e219e22b768b15b5e3d84f619c3fa6c0
Md Istiak Ahmed <istinishat@gmail.com>
2026-05-14 17:01:21 +0700

    update rotbot

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,22 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: "https://bdgovinfo.com/sitemap.xml",
-    host: "bdgovinfo.com",
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "facebookexternalhit",
+        allow: "/",
+      },
+      {
+        userAgent: "Facebot",
+        allow: "/",
+      },
+    ],
+
+    sitemap: "https://www.bdgovinfo.com/sitemap.xml",
+    host: "https://www.bdgovinfo.com",
   };
 }
